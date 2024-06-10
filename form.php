71caa8729b5bc7049dc1697aad8b20347e01b6e9
arnaldorosa <159778135+arnaldorosa@users.noreply.github.com>
2024-06-10 16:38:23 +0200

    first commit

---
+++ form.php
@@ -0,0 +1,14 @@
+<?php
+    
+    $testo = $_GET['paragrafo'];
+    $censura = $_GET['censura'];
+
+    ?>
+
+    <p>Il testo é : <br> <?php echo $testo ?> </p>
+    <span>Lunghezza testo: <?php echo strlen($testo) ?> </span>
+
+    <hr>
+
+    <p>Il testo censurato é : <br> <?php echo str_replace($censura, "***", $testo)?> </p>
+    <span>Lunghezza testo: <?php echo strlen($testo) ?> </span>
---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+        <form action="form.php" method="get"> 
+            <textarea name="paragrafo" cols="35" rows="5" placeholder="Inserisci testo" minlength="4"></textarea>
+            <input type="text" name="censura">
+            <input type="submit" value="invia">
+        </form>
+</body>
+</html>
