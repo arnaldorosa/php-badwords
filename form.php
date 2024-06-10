ed30c20ab21f6408057e7b64067f6dcd88fc585d
arnaldorosa <159778135+arnaldorosa@users.noreply.github.com>
2024-06-10 16:38:58 +0200

    end exercise

--- form.php
+++ form.php
@@ -5,10 +5,10 @@
 
     ?>
 
-    <p>Il testo é : <br> <?php echo $testo ?> </p>
-    <span>Lunghezza testo: <?php echo strlen($testo) ?> </span>
+    <p>Il testo é : <br> <?php echo $testo ?></p>
+    <span>Lunghezza testo: <?php echo strlen($testo) ?></span>
 
     <hr>
 
-    <p>Il testo censurato é : <br> <?php echo str_replace($censura, "***", $testo)?> </p>
-    <span>Lunghezza testo: <?php echo strlen($testo) ?> </span>
+    <p>Il testo censurato é : <br> <?php echo str_replace($censura, "***", $testo)?></p>
+    <span>Lunghezza testo: <?php echo strlen($testo) ?></span>
